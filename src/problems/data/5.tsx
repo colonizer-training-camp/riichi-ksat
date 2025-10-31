5dfb11169b0bf367e3d7d74382bf64a9a543dcfc
최희원 <jeeen1@naver.com>
2025-10-31 17:12:08 +0900

    add problem 5

---
+++ src/problems/data/5.tsx
@@ -0,0 +1,37 @@
+import Viewㄱㄴㄷ from '@/components/Viewㄱㄴㄷ'
+import { ㄱㄴ, ㄱㄴㄷ, ㄴ, ㄴㄷ, ㄷ } from '@/components/macros'
+import type { Problem } from '../types'
+
+const PROBLEM: Problem = {
+  index: 5,
+  score: 2,
+  content: {
+    ko: (
+      <>
+        그림은 어떤 작사가 받은 배패이다. 이와 관련된 설명으로 옳은 것만을
+        &lt;보기&gt;에서 있는 대로 고른 것은?
+        <Viewㄱㄴㄷ>
+          <li>이 배패에는 자패가 모두 8장이 있다.</li>
+          <li>현재 패산에는 ‘1통’이 2장 이하이다.</li>
+          <li>이 작사는 1순에 유국을 선언할 수 없다.</li>
+        </Viewㄱㄴㄷ>
+      </>
+    ),
+    ja: (
+      <>
+        그림은 어떤 작사가 받은 배패이다. 이와 관련된 설명으로 옳은 것만을
+        &lt;보기&gt;에서 있는 대로 고른 것은?
+        <Viewㄱㄴㄷ>
+          <li>이 배패에는 자패가 모두 8장이 있다.</li>
+          <li>현재 패산에는 ‘1통’이 2장 이하이다.</li>
+          <li>이 작사는 1순에 유국을 선언할 수 없다.</li>
+        </Viewㄱㄴㄷ>
+      </>
+    ),
+  },
+  options: [, ㄴ, ㄷ, ㄱㄴ, ㄴㄷ, ㄱㄴㄷ],
+  optionPerRow: 3,
+  answer: 1,
+}
+
+export default PROBLEM
--- src/problems/index.ts
+++ src/problems/index.ts
@@ -2,7 +2,8 @@ import _01 from './data/1'
 import _02 from './data/2'
 import _03 from './data/3'
 import _04 from './data/4'
+import _05 from './data/5'
 
-const PROBLEMS = [_01, _02, _03, _04]
+const PROBLEMS = [_01, _02, _03, _04, _05]
 
 export default PROBLEMS
