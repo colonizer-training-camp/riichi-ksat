6efe1483a20072aee7823a237c62bacc6fedc779
최희원 <jeeen1@naver.com>
2025-10-31 21:45:29 +0900

    add problem 6

--- src/problems/index.ts
+++ src/problems/index.ts
@@ -3,7 +3,8 @@ import _02 from './data/2'
 import _03 from './data/3'
 import _04 from './data/4'
 import _05 from './data/5'
+import _06 from './data/6'
 
-const PROBLEMS = [_01, _02, _03, _04, _05]
+const PROBLEMS = [_01, _02, _03, _04, _05, _06]
 
 export default PROBLEMS
